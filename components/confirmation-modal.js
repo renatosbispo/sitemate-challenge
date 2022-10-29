39ea0ce944fcaedcf0c8ad2de90e35f77a060cd8
Renato Bispo <renatosilvabispo@outlook.com>
2022-10-29 18:57:11 -0300

    feat: implement functionality for the modals

--- components/confirmation-modal.js
+++ components/confirmation-modal.js
@@ -1,14 +1,24 @@
 export const confirmationModalElementName = 'confirmation-modal';
 
 export default class ConfirmationModal extends HTMLElement {
+  cancelButtonId = 'cancel-button';
+  confirmationModalDialogClassName = 'confirmation-modal-dialog';
   confirmationModalTemplateId = 'confirmation-modal';
   defaultDialogMessageContent = 'Are you sure you want to continue?';
   dialogMessageContentSlotName = 'dialog-message-content';
+  yesButtonId = 'yes-button';
+  modalTriggerSelector = '.modal-trigger';
 
   constructor() {
     super();
 
     const shadowRoot = this.attachShadow({ mode: 'open' });
+
+    this.modalClassName = this.className;
+
+    this.triggerButton = document.querySelector(
+      `button.${this.modalClassName}`
+    );
 
     const dialogMessageContent =
       this.dialogMessageContent || this.defaultDialogMessageContent;
@@ -29,14 +39,46 @@ export default class ConfirmationModal extends HTMLElement {
     const confirmationModalTemplateClone =
       confirmationModalTemplate.content.cloneNode(true);
 
-    document
-      .querySelector(confirmationModalElementName)
-      .appendChild(dialogMessageContentSpan);
-
     shadowRoot.appendChild(confirmationModalTemplateClone);
-  }
 
-  // connectedCallback() {}
+    this.dialog = shadowRoot.querySelector('dialog');
+
+    this.yesButton = shadowRoot.getElementById(this.yesButtonId);
+
+    this.cancelButton = shadowRoot.getElementById(this.cancelButtonId);
+
+    if (typeof this.dialog.showModal !== 'function') {
+      this.dialog.hidden = true;
+    }
+
+    this.triggerButton.addEventListener('click', () => {
+      if (typeof this.dialog.showModal === 'function') {
+        this.dialog.showModal();
+      } else {
+        alert('Sorry, the <dialog> API is not supported by this browser.');
+      }
+    });
+
+    this.yesButton.addEventListener('click', () => {
+      if (this.yesButtonAction) {
+        this.yesButtonAction();
+      } else {
+        console.log('clicked yes button');
+      }
+    });
+
+    this.cancelButton.addEventListener('click', () => {
+      if (this.cancelButtonAction) {
+        this.cancelButtonAction();
+      } else {
+        console.log('clicked cancel button');
+      }
+    });
+
+    document
+      .querySelector(`${confirmationModalElementName}.${this.modalClassName}`)
+      .appendChild(dialogMessageContentSpan);
+  }
 
   get dialogMessageContent() {
     return this.getAttribute('dialog-message-content');
--- main.js
+++ main.js
@@ -4,8 +4,40 @@ import ConfirmationModal, {
 
 import { injectHtmlTemplates } from './modules/utils.js';
 
+const defaultYesButtonAction = (modalClassName) => {
+  const output = document.querySelector(`output.${modalClassName}`);
+  console.log(output);
+  output.innerHTML = 'You just clicked "Yes"';
+};
+
+const defaultCancelButtonAction = (modalClassName) => {
+  const output = document.querySelector(`output.${modalClassName}`);
+
+  output.innerHTML = 'You just clicked "Cancel"';
+};
+
 (async () => {
   await injectHtmlTemplates();
 
   customElements.define(confirmationModalElementName, ConfirmationModal);
+
+  const confirmationModal1 = document.querySelector(
+    `${confirmationModalElementName}.confirmation-modal-1`
+  );
+
+  confirmationModal1.yesButtonAction = () =>
+    defaultYesButtonAction('confirmation-modal-1');
+
+  confirmationModal1.cancelButtonAction = () =>
+    defaultCancelButtonAction('confirmation-modal-1');
+
+  const confirmationModal2 = document.querySelector(
+    `${confirmationModalElementName}.confirmation-modal-2`
+  );
+
+  confirmationModal2.yesButtonAction = () =>
+    defaultYesButtonAction('confirmation-modal-2');
+
+  confirmationModal2.cancelButtonAction = () =>
+    defaultCancelButtonAction('confirmation-modal-2');
 })();
